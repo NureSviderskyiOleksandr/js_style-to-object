3f3d12bdfdaa8933a25469ce20716477dc38ac5c
Oleksandr <oleksandr.sviderskyi@nure.ua>
2026-06-20 01:10:21 +0300

    renamed variable

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,15 +6,15 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const arrayOfLines = sourceString.split(';');
-  const convertableString = arrayOfLines.reduce((prev, line) => {
+  const styleDeclarations = sourceString.split(';');
+  const convertableString = styleDeclarations.reduce((stylesObject, line) => {
     const [key, value] = line.trim().split(':');
 
     if (key && value) {
-      prev[key.trim()] = value.trim();
+      stylesObject[key.trim()] = value.trim();
     }
 
-    return prev;
+    return stylesObject;
   }, {});
 
   return convertableString;
