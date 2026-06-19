616f7658deedf676a88648e2933e8ae530881ee9
Oleksandr <oleksandr.sviderskyi@nure.ua>
2026-06-20 00:50:55 +0300

    complete task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arrayOfLines = sourceString.split(';');
+  const result = {};
+
+  for (let i = 0; i < arrayOfLines.length; i++) {
+    const line = arrayOfLines[i].trim().split(':');
+
+    if (line[0] === undefined || line[0].length < 1) {
+      continue;
+    }
+
+    if (line[1] === undefined || line[1].length < 1) {
+      continue;
+    }
+
+    result[line[0].trim()] = line[1].trim();
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
